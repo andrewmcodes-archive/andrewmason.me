bd6f29c57c27df196052027355932257aec45e2f
Andrew Mason <andrew.mason@n2pub.com>
2019-01-07 23:47:05 -0500

    style changes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,23 +4,23 @@ import { FaGithub } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa";
 import { FaTwitterSquare } from "react-icons/fa";
 const Footer = () => (
-  <div className="bg-grey-lighter shadow-inner">
-    <div className="flex flex-col-reverse md:flex-row md:justify-between max-w-xl mx-auto p-4 md:p-8 text-sm">
+  <div className="bg-grey-lighter shadow-inner ">
+    <div className="align-baseline flex flex-col-reverse md:flex-row md:justify-between max-w-xl mx-auto p-4 md:p-8 md:pb-2 text-sm">
       <p className="w-full md:w-3/4 text-center md:text-left mt-4 sm:mt-0 text-indigo-dark">
         © 2018{" "}
         <a
           href="https://andrewmason.me"
-          className="no-underline text-indigo-darkest"
+          className="no-underline text-indigo-darkest hover:text-blue mb-0"
           target="_blank"
           rel="noopener"
         >
           Andrew Mason
         </a>
       </p>
-      <ul className="md:w-1/4 list-reset flex justify-between text-base md:text-2xl h-full">
+      <ul className="w-full md:w-1/4 list-reset flex justify-between text-xl md:text-2xl h-full">
         <li className="px-1 md:px-2">
           <a
-            className="flex flex-col text-center text-indigo-darker no-underline font-semibold"
+            className="flex flex-col text-center text-indigo-darker hover:text-blue no-underline font-semibold"
             href="mailto:masonam96@outlook.com?Subject=Hello!"
             target="_blank"
             rel="noopener"
@@ -30,7 +30,7 @@ const Footer = () => (
         </li>
         <li className="px-1 md:px-2">
           <a
-            className="flex flex-col text-center text-indigo-darker no-underline font-semibold"
+            className="flex flex-col text-center text-indigo-darker hover:text-blue no-underline font-semibold"
             href="https://www.linkedin.com/in/andrew-mason"
             target="_blank"
             rel="noopener"
@@ -40,7 +40,7 @@ const Footer = () => (
         </li>
         <li className="px-1 md:px-2">
           <a
-            className="flex flex-col text-center text-indigo-darker no-underline font-semibold"
+            className="flex flex-col text-center text-indigo-darker hover:text-blue no-underline font-semibold"
             href="https://twitter.com/andrewmcodes"
             target="_blank"
             rel="noopener"
@@ -50,7 +50,7 @@ const Footer = () => (
         </li>
         <li className="pl-1 md:pl-2">
           <a
-            className="flex flex-col text-center text-indigo-darker no-underline font-semibold"
+            className="flex flex-col text-center text-indigo-darker hover:text-blue no-underline font-semibold"
             href="https://github.com/andrewmcodes"
             target="_blank"
             rel="noopener"
